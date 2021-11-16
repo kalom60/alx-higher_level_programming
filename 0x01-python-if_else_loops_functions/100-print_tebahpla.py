ad3a5290252898c320c5a7abb917267c90dea85e
kalom60 <kaleabtesfaye00@gmail.com>
2021-11-16 09:05:21 -0800

    a python script that prints the ASCII alphabet, in reverse order, alternating lowercase and uppercase

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
 for string in reversed(range(97, 123)):
-    print("{:c}".format(string if string % 2 == 0 else string - 32), end='')
+    if string % 2 != 0:
+        string = string - 32
+    print("{:c}".format(string), end='')
