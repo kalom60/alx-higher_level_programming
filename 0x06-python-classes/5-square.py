778e34a0bf2ea99c98bb97dca0260e53f672d080
kalom60 <kaleabtesfaye00@gmail.com>
2021-12-07 06:03:23 -0800

    a class Square that defines a square by: (based on 4-square.py)

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,6 +1,24 @@
 #!/usr/bin/python3
+"""Defines a class Square"""
+
+
 class Square:
+    """Represents a square
+
+    Attributes:
+        __size (int): size of a side of the square
+    """
+
     def __init__(self, size=0):
+        """initializes the square
+
+        Args:
+            size (int): size of a side of the square
+
+        Returns:
+            None
+        """
+
         if isinstance(size, int) and size >= 0:
             self.__size = size
         elif not isinstance(size, int):
@@ -10,10 +28,25 @@ class Square:
 
     @property
     def size(self):
+        """getter of __size
+	
+        Returns:
+            The size of the square
+        """
+
         return self.__size
 
     @size.setter
     def size(self, value):
+        """setter of __size
+
+        Args:
+            value (int): size of a side of the square
+
+        Returns:
+            None
+        """
+
         if isinstance(value, int) and value >= 0:
             self.__size = value
         elif not isinstance(value, int):
@@ -22,9 +55,21 @@ class Square:
             raise ValueError("size must be >= 0")
 
     def area(self):
+        """calculates the square's area
+
+        Returns:
+            The area of the square
+        """
+
         return self.__size ** 2
 
     def my_print(self):
+        """prints the square
+
+        Returns:
+            None
+        """
+
         if not self.__size:
             print()
         else:
