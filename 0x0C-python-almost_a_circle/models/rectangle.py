4979a72d0b58fd3af5991f2a011cefafcf09fa7a
kalom60 <kaleabtesfaye00@gmail.com>
2021-12-30 02:45:37 -0800

    A python rectangle class

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python3
+"""Define a rectangle class"""
+from models.base import Base
+
+
+class Rectangle(Base):
+    """Represent a rectangle"""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Initialize a new Rectangle
+
+        Args:
+            width  (int): width of the rectangle
+            height (int): height of the rectangle
+            x      (int): x coordinater of the rectangle
+            y      (int): y coordinater of the rectangle
+            id     (int): id of the rectangle
+
+        Raises:
+            TypeError: If either of width or height is not an int
+            ValueError: If either of width or height <= 0
+            TypeError: If either of x or y is not an int
+            ValueError: If either of x or y < 0
+        """
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """Get the width of the rectangle"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Set the width of the rectangle"""
+        self.__width = value
+
+    @property
+    def height(self):
+        """Get the height of the rectangle"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Set the height of the rectangle"""
+        self.__height = value
+
+    @property
+    def x(self):
+        """Get the x coordinater of the rectangle"""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """Set the x coordinater of the rectangle"""
+        self.__x = value
+
+    @property
+    def y(self):
+        """Get the y coordinater of the rectangle"""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """Set the y coordinater of the rectangle"""
+        self.__y = value
