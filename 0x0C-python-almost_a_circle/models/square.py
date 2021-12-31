09aa8d259793e8825ebf5f6afa49cf68549f428e
kalom60 <kaleabtesfaye00@gmail.com>
2021-12-31 07:16:38 -0800

    Update the class Square by adding the public method def update(self, *args, **kwargs) that assigns attributes

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -33,3 +33,45 @@ class Square(Rectangle):
                                                  self.x,
                                                  self.y,
                                                  self.width)
+
+    def update(self, *args, **kwargs):
+        """Update the square
+
+        Args:
+            *args (ints): new attribute values
+                - 1st argument should be the id attribute
+                - 2nd argument should be the size attribute
+                - 3rd argument should be the x attribute
+                - 4th argument should be the y attribute
+            **kwargs(dict): New key-value pairs of attributes
+        """
+        if args and len(args) != 0:
+            if args is None:
+                self.__init__(self.width, self.height, self.x, self.y)
+            elif len(args) == 1:
+                self.id = args[0]
+            elif len(args) == 2:
+                self.id = args[0]
+                self.size = args[1]
+            elif len(args) == 3:
+                self.id = args[0]
+                self.size = args[1]
+                self.x = args[2]
+            elif len(args) == 4:
+                self.id = args[0]
+                self.size = args[1]
+                self.x = args[2]
+                self.y = args[3]
+        elif kwargs and len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == 'id':
+                    if v is None:
+                        self.__init__(self.width, self.height, self.x, self.y)
+                    else:
+                        self.id = v
+                elif k == 'size':
+                    self.size = v
+                elif k == 'x':
+                    self.x = v
+                elif k == 'y':
+                    self.y = v
