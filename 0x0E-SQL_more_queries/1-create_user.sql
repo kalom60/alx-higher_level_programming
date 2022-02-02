3aa7df8bbf65fe4b1a43c7798d0800b62441df4f
kalom60 <kaleabtesfaye00@gmail.com>
2022-02-02 03:07:49 -0800

    Fix error

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -5,7 +5,7 @@ CREATE USER
     IDENTIFIED BY 'user_0d_1_pwd';
 
 GRANT ALL PRIVILEGES
-    ON *.*
+    ON * . *
     TO 'user_0d_1'@'localhost'
     IDENTIFIED BY 'user_0d_1_pwd';
 FLUSH PRIVILEGES;
