566278f7315ca6156b2df9a78d4859eb3502bf33
kalom60 <kaleabtesfaye00@gmail.com>
2022-02-28 16:05:47 +0300

    fix semistandard

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,16 +1,14 @@
 #! /usr/bin/node
 
 if (process.argv.length < 4) {
-    console.log('0');
-} 
-else {
+  console.log('0');
+} else {
+  const size = process.argv.length;
+  const myArray = [];
+  for (let x = 2; x < size; x++) {
+    myArray[x - 2] = parseInt(process.argv[x]);
+  }
 
-    const size = process.argv.length;
-    const myArray = [];
-    for (let x = 2; x < size; x++) {
-        myArray[x - 2] = parseInt(process.argv[x]);
-    }
-
-    myArray.sort();
-    console.log(myArray.at(-2));
+  myArray.sort();
+  console.log(myArray.at(-2));
 }
