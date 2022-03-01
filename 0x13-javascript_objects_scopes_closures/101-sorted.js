ab7f0522d8ddaa242fa8a86542101c731fc48f18
kalom60 <kaleabtesfaye00@gmail.com>
2022-03-01 15:35:31 +0300

    fix semistandard

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -3,9 +3,9 @@
 const { dict } = require('./101-data.js');
 const newDict = {};
 for (const N in dict) {
-    if (newDict[dict[N]] === undefined) {
-	newDict[dict[N]] = [];
-    }
-    newDict[dict[N]].push(N);
+  if (newDict[dict[N]] === undefined) {
+    newDict[dict[N]] = [];
+  }
+  newDict[dict[N]].push(N);
 }
 console.log(newDict);
