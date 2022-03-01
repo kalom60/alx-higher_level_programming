24085216044a41e899585273342c928b6f6bab42
kalom60 <kaleabtesfaye00@gmail.com>
2022-03-01 14:39:38 +0300

    fix semistandard

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
 exports.nbOccurences = function (list, searchElement) {
-  let nb = 0;
+  let num = 0;
   for (let i = 0; i < list.length; i++) {
     if (list[i] === searchElement) {
-      nb++;
+      num++;
     }
   }
-  return nb;
+  return num;
 };
