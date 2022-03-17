9b11a8b7dbe29394b15e5d1589a5e5854190beff
kalom60 <kaleabtesfaye00@gmail.com>
2022-03-17 23:48:25 +0300

    fix error

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that makes a request to causes an specific response
-curl -sX POST 0.0.0.0:5000/catch_me -H "Content-Type: text/html" -d 'You got me!'
+curl -sLX PUT -H "Origin: You got me!" 0.0.0.0:5000/catch_me
