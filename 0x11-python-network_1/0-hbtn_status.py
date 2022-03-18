2aef27f0401f6843441cdadf2ec5e305ed23b9d1
kalom60 <kaleabtesfaye00@gmail.com>
2022-03-18 22:36:31 +0300

    fix pycodestyle

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,10 +2,11 @@
 """ Python script that fetches https://intranet.hbtn.io/status """
 
 if __name__ == "__main__":
-    import urllib.request as request
-    with request.urlopen('https://intranet.hbtn.io/status') as r:
-        html = r.read()
-        print('Body response:')
-        print("\t- type: {}".format(type(html)))
-        print("\t- content: {}".format(html))
-        print("\t- utf8 content: {}".format(html.decode('utf-8')))
+    from urllib.request import urlopen
+
+    with urlopen('https://alx-intranet.hbtn.io/status') as res:
+        result = res.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(result)))
+        print("\t- content: {}".format(result))
+        print("\t- utf8 content: {}".format(result.decode("utf-8")))
--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,4 +1,4 @@
-#!/opt/homebrew/bin/python3
+#!/usr/bin/python3
 """
 a python script that takes in a URL and an email, sends a POST
 request to the passed URL with the email as a parameter, and
