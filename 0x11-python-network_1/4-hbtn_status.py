1f5696947b9efdce59fefa365ed0e75816881122
kalom60 <kaleabtesfaye00@gmail.com>
2022-03-18 22:49:51 +0300

    fix error

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -4,10 +4,9 @@ fetches https://intranet.hbtn.io/status
 """
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import requests
-    r = requests.get('https://intranet.hbtn.io/status')
-    text = r.text
+    r = requests.get('https://alx-intranet.hbtn.io/status')
     print("Body response:")
-    print("\t- type: {}".format(type(text)))
-    print("\t- content: {}".format(text))
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
