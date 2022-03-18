26c031e428e4836ed490fc5d49e42a3d3606fef5
kalom60 <kaleabtesfaye00@gmail.com>
2022-03-18 22:35:36 +0300

    a Python script that takes in a URL and an email, sends a POST request to the passed URL with the email as a parameter, and displays the body of the response (decoded in utf-8)

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,17 @@
+#!/opt/homebrew/bin/python3
+"""
+a python script that takes in a URL and an email, sends a POST
+request to the passed URL with the email as a parameter, and
+displays the body of the response
+"""
+if __name__ == "__main__":
+    from urllib import request, parse
+    import sys
+    url = sys.argv[1]
+    value = {'email': sys.argv[2]}
+    data = parse.urlencode(value)
+    data = data.encode("utf-8")
+    req = request.Request(url, data)
+    with request.urlopen(req) as resp:
+        new = resp.read()
+        print(new.decode("utf-8"))
