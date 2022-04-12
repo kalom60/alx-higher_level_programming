394df4c011566ad2b1481102a5f78db2ef2a9be4
kalom60 <39136564+kalom60@users.noreply.github.com>
2022-04-12 13:23:21 +0300

    a script that reads and prints the content of a file

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,9 @@
+#!/opt/homebrew/bin/node
+const fs = require('fs');
+fs.readFile(process.argv[2], 'utf8', function (err, data) {
+  if (data === undefined) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+});
