225a17492e1f3649c5da45123d45a415d91c2be5
kalom60 <39136564+kalom60@users.noreply.github.com>
2022-04-20 13:34:26 +0300

    fix style

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -2,6 +2,6 @@ const $ = window.$;
 $.get(
   'https://swapi-api.hbtn.io/api/people/5/?format=json',
   function (data, status) {
-    $('div#character').text(data.name);
+    $('DIV#character').text(data.name);
   }
 );
